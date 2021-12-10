2c1817868b8eed157f60d84291a2e76a7eaecf3f
Shaquille Rivera <shaq.rivera@gmail.com>
2021-12-09 20:32:40 -0500

    Populated recipe results onto the page and allowed the user to rate the recipe. Also logged the recipe to the user's local storage

--- develop/js/script.js
+++ develop/js/script.js
@@ -5,15 +5,22 @@ var diningInChoiceEl = $("#diningInChoice");
 var eatingOutChoiceEl = $("#eatingOutChoice");
 var recipeRatingEl = $("#recipeRating");
 var restaurantRatingEl = $("#restaurantRating");
-var APIKey = '94a7e955bd8b4bbdbd0a552e1666b62b';
+var spoonAPIKey = '94a7e955bd8b4bbdbd0a552e1666b62b';
+var yelpAPIKey = 'QK9VRlMeHsRVRkocggvEgEpTj0UlSioqD2c69TNDAE6LotIPGH46MU5hfG1vlQ_m-xJA25EEhJfkp1-hsnKjNtniB-ROWHnR_HpY3h1T-poiQKhq21pAcGKaJJyyYXYx'
 var diningInInputEl = $('#diningInInput');
 var diningInSearchEl = $('#diningInSearch');
+var recipeResultsEl = $('#recipeResults');
+var declineRecipeEl = $('#declineRecipe');
+var acceptRecipeEl = $('#acceptRecipe');
+var diningInMoodEl = $('#diningInMood');
+var recipeRatingSubmitEl = $('#recipeRatingSubmit');
 
 //Remove elements from view
 diningInEl.remove();
 eatingOutEl.remove();
 restaurantRatingEl.remove();
 recipeRatingEl.remove();
+recipeRatingSubmitEl.remove();
 
 function displayDiningInEl () {
     $("body").append(diningInEl);
@@ -28,18 +35,49 @@ function displayEatingOutEl () {
 
 function searchRecipes (){
     var diningInInputVal = diningInInputEl.val().replace(' ',',');
-    var spoonacularLink = 'https://api.spoonacular.com/recipes/complexSearch?apiKey='+APIKey+'&query='+diningInInputVal;
+    var spoonacularLink = 'https://api.spoonacular.com/recipes/complexSearch?apiKey='+spoonAPIKey+'&query='+diningInInputVal;
     fetch(spoonacularLink)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
+        recipeResultsEl.empty();
+        var recipeInformationLink = 'https://api.spoonacular.com/recipes/'+data.results[[Math.floor(Math.random()*data.results.length)]].id+'/information?apiKey='+spoonAPIKey;
+        fetch(recipeInformationLink)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            recipeResultsEl.append('<h1>'+data.title+'</h1>' +data.instructions)
+            for (let index = 0; index < data.extendedIngredients.length; index++) {
+                recipeResultsEl.append('<li>'+data.extendedIngredients[index].original+'</li>');
+                
+            }
+            function submitRecipeRating(){
+                localStorage.setItem(data.title,recipeRatingEl.val())  
+            }
+            recipeRatingSubmitEl.on('click', submitRecipeRating);
+            
+        })
         
-        console.log(data);
     })
 }
 
+function showRecipeRating() {
+    acceptRecipeEl.remove();
+    declineRecipeEl.remove();
+    diningInMoodEl.remove();
+    diningInEl.append(recipeRatingEl);
+    diningInEl.append(recipeRatingSubmitEl);
+    
+};
+
+
 //Add event listeners to buttons
-diningInChoiceEl.on("click", displayDiningInEl);
+
 eatingOutChoiceEl.on("click", displayEatingOutEl);
-diningInSearchEl.on('click', searchRecipes)
+diningInSearchEl.on('click', searchRecipes);
+diningInChoiceEl.on("click", displayDiningInEl);
+declineRecipeEl.on('click',searchRecipes);
+acceptRecipeEl.on('click', showRecipeRating);
